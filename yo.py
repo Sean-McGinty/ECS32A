113ef422e5137d3571396f8d5c9dd0079ab9e276
Sean-McGinty <62928856+Sean-McGinty@users.noreply.github.com>
2020-04-12 13:48:29 -0700

    Delete yo.py

--- yo.py
+++
@@ -1,6 +0,0 @@
-#yo.py
-#ECS32A
-#
-#Prints Yo followed by a user-inputed name. It then prints the question: How's it going?
-name=input("What's your name?") #Creates a varable for the user's name
-print("Yo",name+"!!\n"+"How's it going?")
