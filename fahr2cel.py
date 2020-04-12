a7735c8e370ffb5e38f360c1b7fc36a3e78cefc0
Sean-McGinty <62928856+Sean-McGinty@users.noreply.github.com>
2020-04-12 13:52:15 -0700

    Delete fahr2cel.py

--- fahr2cel.py
+++
@@ -1,7 +0,0 @@
-#fahr2cel.py
-#ECS32A
-#
-#Convert Fahrenheit to Celcius
-fahrenheit = 32
-celsius = (fahrenheit - 32.0) * (5 / 9) #Performs the conversion of Fahrenheit to Celcius
-print(fahrenheit, "Fahrenheit is", celsius, "Celsius.") #Prints Celcius Value
