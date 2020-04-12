daa429b282313b6bc5355ad3671f28f0bf9849d6
Sean-McGinty <62928856+Sean-McGinty@users.noreply.github.com>
2020-04-12 13:52:34 -0700

    Delete converter.py

--- converter.py
+++
@@ -1,8 +0,0 @@
-#converter.py
-#ECS32A
-#
-#Outputs integer and binary representations of a user-inputed character
-character=input("Enter a character:") #Creates a Variable of a user-inputed character
-number=int(ord(character)) #Creates a variable of integer of that chararcter
-binary=bin(number) #Creates a variable of the binary representation of that character
-print(character,"corresponds to the integer",number,"which is",binary,"in binary.")
