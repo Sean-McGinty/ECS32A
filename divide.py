5cdd110f8ac29aa50dbdca22ea0890ce80eb4038
Sean-McGinty <62928856+Sean-McGinty@users.noreply.github.com>
2020-04-12 13:52:55 -0700

    Delete divide.py

--- divide.py
+++
@@ -1,9 +0,0 @@
-#divide.py
-#ECS32A
-#
-#Integer Division Calculator
-Dividend=int(input("Enter a number:")) #User inputs Dividend and Saves as a Variable
-Divisor=int(input("Enter a number to divide that by:")) #User inputs Divisor and Saves as a Variable
-quotient=Dividend//Divisor #Performs Floored Division of Dividend/Divisor
-remainder=Dividend%Divisor #Calculatates Remainder after Division
-print(Dividend,"divided by",Divisor,"is",quotient,"with",remainder,"remaining")
