040a2c809a0f2e6842a2d0ceea04ddcd40637d2d
Sean-McGinty <62928856+Sean-McGinty@users.noreply.github.com>
2020-04-12 13:49:15 -0700

    Delete stim2.py

--- stim2.py
+++
@@ -1,8 +0,0 @@
-#stim2.py
-#ECS32A
-#
-#Household Stimulus Check Calculation
-number_adults=int(input("How many adults?")) #Creates a Variable for number of adults in a family
-number_kids=int(input("How many kids?")) #Creates a variable for number of kids in a family
-check_amount=str(500*number_kids+1200*number_adults) #Creates variable for the check amount to be recieved by the family
-print("You will get a check for $"+check_amount+".")
